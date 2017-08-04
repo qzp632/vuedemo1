9a02df901e871ccf6276aa1061f1319df8a9c4a3
qzp <qzp@qzpdeMBP.lan>
2017-08-04 15:26:16 +0800

    添加了子传父功能

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="list">
-    <ul>
+  <sons @btnv="bus"></sons>
+    <ul v-show="flag">
       <li v-for="(item, index) in filmData" @click="getDeyali(item,index)">
         <span>{{item.collect_count}}</span>
         <p>{{item.original_title}}</p>
       </li>
     </ul>
-
+    
   </div>
 </template>
 
 <script>
+import sons from '@/components/sons'
 import {mapMutations, mapGetters} from 'vuex'
 import { listF } from '../comm/comm'
 export default {
@@ -23,6 +25,11 @@ export default {
   created() {
     this._init();
   },
+  data() {
+    return {
+      flag : false
+    }
+  },
   methods: {
     _init() {
       listF("movie/in_theaters").then( res => {
@@ -34,12 +41,15 @@ export default {
         this.getitemFile(item)
         this.$router.push("/detali")
     },
+    bus(key) {
+      this.flag = key
+    },
     ...mapMutations({
       getfilmdata: 'GET_FILMDATA',
       getitemFile: 'GET_ITEMFILE'
     })
-  }
-  
+  },
+  components: { sons }
 }
 </script>
 
---
+++ src/components/sons.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="list">
+    <div>aaaaaa</div>
+    <div>aaaaaa</div>
+    <div>aaaaaa</div>
+    <div>aaaaaa</div>
+    <button @click="btn">点击我就行</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters([
+      'change'
+    ]),      
+  },
+  data() {
+    return {
+      flag:false
+    }
+  },
+
+  methods: {
+    btn() {
+      this.flag = !this.flag
+      this.getchange(this.flag)
+      this.$emit('btnv',this.change)
+    },
+    ...mapMutations({
+      getchange: 'GET_CHANGE'
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
